a49b79aeb16b2b8d6dc6f59ec45b45aecbbf8867
Daiki Kinoshita <58548429+Daiki-knst@users.noreply.github.com>
2020-05-24 17:01:45 +0900

    mario風gameのcodeを追加

---
+++ Assets/EnemyController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    float speed = 2.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
+        if (this.transform.position.x > 3)
+        {
+            speed *= -1;
+        }
+        if(this.transform.position.x<-3)
+        {
+            speed *= -1;
+        }
+        
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{   //変数を宣言
+    float speed = 10.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Z軸方向に進み続ける
+        this.transform.position += new Vector3 (0, 0, speed * Time.deltaTime);
+
+        //右矢印キーを押している間
+           if(Input.GetKey("right")==true)
+        {
+            //X軸方向へ動く
+            this.transform.position += new Vector3 (speed * Time.deltaTime, 0, 0);
+        }
+        //左矢印キーを押している間
+        if(Input.GetKey("left") == true)
+        {
+            //X軸方向へ動く
+            this.transform.position -= new Vector3 (speed * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKeyDown("up") == true)
+        {
+            //ジャンプする
+            this.transform.position += new Vector3(0, 4, 0);
+        }
+
+    }
+    //衝突したときに呼び出される
+    void OnCollisionEnter(Collision col)
+    {
+        //Enemyタグと衝突したとき
+        if (col.gameObject.tag == "Enemy")
+        {
+            Debug.Log("Hit Enemy");
+            this.transform.localScale *= 0.5f; //自分のScaleを0.5倍に
+            this.transform.position = new Vector3(0, 0, 0);
+        }
+        if (col.gameObject.tag == "Goal")
+        {
+            Debug.Log("Goal!!!");
+            this.transform.localScale *= 1.5f;
+            this.transform.position = new Vector3(0, 0, 0);
+        }
+    }
+}
